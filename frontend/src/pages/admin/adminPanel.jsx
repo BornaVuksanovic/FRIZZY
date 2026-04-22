d1a995928335c82b28259359f83ac1940b04b1b8
BornaVuksanovic <boco.vuksanovic2@gmail.com>
2026-04-22 13:41:29 +0200

    display services on admin panel

--- frontend/src/pages/admin/adminPanel.jsx
+++ frontend/src/pages/admin/adminPanel.jsx
@@ -3,41 +3,67 @@ import { useState, useEffect } from "react";
 import { useNavigate, Link } from "react-router-dom";
 import axios from "axios";
 import { ClipLoader } from "react-spinners"
-
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 
 export default function AdminPanel() {
     const { token, user, logout } = useAuthStore();
-    const [hairdressers, setHairdressers] = useState([]);
-    const [isLoading, setIsLoading] = useState(true);
 
-    const getHairdressers = async () => {
-        setIsLoading(true);
-        try {
-            const response = await axios.get("http://localhost:1000/api/app/getHairdressers", {
-                headers: {
-                    Authorization: `Bearer ${token}`
-                }
-            });
-            setHairdressers(response.data.hairdressers);
-            console.log("uspjesno dohvaceni frizeri")
-            
-        } catch (error) {
-            console.log("Error getting hairdressers");
-        }finally{
-            setIsLoading(false);
-        }
+
+  const hairdressersQuery = useQuery({
+    queryKey: ['hairdressers'],
+    queryFn: async () => {
+      const response = await axios.get("http://localhost:1000/api/app/getHairdressers",{   
+          headers: {
+            Authorization: `Bearer ${token}`
+          }
+      });
+      return response.data.hairdressers;
+    },
+    
+    onError: (error) => {
+      console.error("Get hairdressers failed", error.message);
+      toast.error('Neuspješno dohvacanje frizera');
+    },
+    
+    enabled: () => {
+     !!user  // Pokreni tek kad imamo admina
     }
+    
+  });
 
-    useEffect(()=>{
-        getHairdressers();
-    },[])
+  const servicesQuery = useQuery({
+    queryKey: ['services'],
+    queryFn: async () => {
+      const response = await axios.get("http://localhost:1000/api/app/getServices",{   
+          headers: {
+            Authorization: `Bearer ${token}`
+          }
+      });
+      return response.data.services;
+    },
+    
+    onError: (error) => {
+      console.error("Get services failed", error.message);
+      toast.error('Neuspješno dohvacanje uslugas');
+    },
+    
+    enabled: () => {
+     !!user  // Pokreni tek kad imamo admina
+    }
+    
+  });
 
-    if ( isLoading || !hairdressers){
-        return (
-            <div>
-                <ClipLoader loading={isLoading} size={50} color={"#3498db"} />
-            </div>
-            );
+  const services = servicesQuery.data;
+  const hairdressers = hairdressersQuery.data;
+
+  const isLoading = servicesQuery.isLoading || hairdressersQuery.isLoading || !user;
+
+    if (isLoading) {
+      return (
+          <div>
+              <ClipLoader loading={isLoading} size={50} color={"#3498db"} />
+          </div>
+      );
     }
 
 
@@ -47,7 +73,13 @@ export default function AdminPanel() {
         <h1>ADMIN</h1>
             <h2>Panel</h2>
             <h3>Zaposlenici:</h3>
-            {hairdressers?.map(h => <p key={h.id}>{h.firstName} {h.lastName}</p>)}    
+            <div>
+                {hairdressers?.map(h => <p key={h.id}>{h.firstName} {h.lastName}</p>)} 
+            </div>
+            <h3>Usluge:</h3>
+            <div>
+                {services?.map(s => <p key={s.id}>{s.name} - {s.duration}min {s.price}€</p>)}
+            </div>
         </div>
         <div>
             <button onClick={logout}>
