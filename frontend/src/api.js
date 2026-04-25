dcae24c3b3e953a9fb621c5ff84e1737a1adb05b
BornaVuksanovic <boco.vuksanovic2@gmail.com>
2026-04-25 13:19:07 +0200

    modify api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,9 +1,12 @@
-import axios from "axios";
+import axios from 'axios';
 
-const BASE_URL = __DEV__ ? "http://localhost:1000" : "https://frizzy.onrender.com" ;
+
+const BASE_URL = import.meta.env.DEV 
+  ? "http://localhost:5000" 
+  : "https://frizzy.onrender.com";
 
 const api = axios.create({
-  baseURL: BASE_URL
+  baseURL: BASE_URL,
 });
 
 export default api;
